48b31ef28b4f2df0d6c50f7c3e733b70d13fdeb4
John Lynch <johnlynch.32768@gmail.com>
2019-12-15 05:11:40 +0000

    Added part() and parts() lambdas.

--- partitioning.py
+++ partitioning.py
@@ -23,3 +23,10 @@ def partition(arr, func):
 from math import ceil, sqrt
 primes = lambda k: [2] + sorted(partition(range(2, k), lambda n: all(n % i for i in ([2] + list(range(3, ceil(sqrt(n)) + 1, 2)))))[1])
 
+part = lambda arr, func: [[*set([x for x in arr if func(x) == key])] for key in set(map(func, arr))]
+# >>> part(primes(100), lambda n: n % 10)
+# [[71, 41, 11, 61, 31], [2], [3, 73, 43, 13, 83, 53, 23], [5], [97, 67, 37, 7, 47, 17], [79, 19, 89, 59, 29]]
+# >>> part(primes(100), lambda n: n // 10)
+[[2, 3, 5, 7], [19, 17, 11, 13], [29, 23], [37, 31], [41, 43, 47], [59, 53], [67, 61], [73, 79, 71], [89, 83], [97]]
+
+parts = lambda arr, func: [sorted([*set([x for x in arr if func(x) == key])]) for key in set(map(func, arr))]
